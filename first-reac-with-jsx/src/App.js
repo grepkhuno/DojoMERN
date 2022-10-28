7492d49c699f09062bdbb3f50c16b50ed22d0631
Eliott <grepkhuno@gmail.com>
2022-10-27 22:16:34 -0400

    second commit

---
+++ first-reac-with-jsx/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <li>Learn React</li>
+      <li>Climb MT. Everest</li>
+      <li>Run a Marathon</li>
+      <li>Feed the dogs</li>
+    </div>
+  );
+}
+
+export default App;
