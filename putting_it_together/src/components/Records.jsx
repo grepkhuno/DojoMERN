1189b280ad18b02d8beca21ef527c54d11a10b27
Eliott <grepkhuno@gmail.com>
2022-11-05 10:57:17 -0400

    pu it together

--- putting_it_together/src/components/Records.jsx
+++ putting_it_together/src/components/Records.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Records(props) {
-  const { name, lastName, hairColor, age } = props;
+  const { name, lastName, hairColor } = props;
+  const [ age, setAge ] = useState(props.age);
   return (
   
       <div className='d-flex flex-column p-2 '><h2 className='col-auto mx-auto'> {lastName}, {name}</h2>
         <p className='col-auto mx-auto'>Age: {age}</p>
         <p className='col-auto mx-auto'>Hair Color: {hairColor}</p>
-        <button  className='col-auto mx-auto btn btn-primary' type="button">Bithday Button for {lastName}, {name}</button>
+        <button  onClick={()=> setAge(age+1)} className='col-auto mx-auto btn btn-primary' type="button">Bithday Button for {lastName}, {name}</button>
       </div>
 
   )
