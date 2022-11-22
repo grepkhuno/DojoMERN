24aa1e380b5136c374c6c002ef6b7a61d8d9c78e
Eliott <grepkhuno@gmail.com>
2022-11-21 22:03:34 -0500

    pokemon  api

--- axios_pokemon/src/App.js
+++ axios_pokemon/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react'
 function App() {
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+      .then((res) => {
+        return res.json()
+      }).then((result) => {
+        console.log(result)
+        setList(result.results)
+      }).catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      {
+        
+        list.map((user) => (
+         
+            <ul className="list-group">
+              <li className="list-group-item">
+                <h1>{user.name} </h1>
+              </li>
+            </ul>
+         
+
+        ))
+      }
+   
     </div>
   );
 }
-
 export default App;
